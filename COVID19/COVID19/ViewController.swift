de41a81ddc96f51cb0150b0eefbc6e4e6416b934
Haedong-Jeon <21212004@ynu.ac.kr>
2020-02-27 17:33:27 +0900

    프로젝트 시작

---
+++ COVID19/COVID19/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  COVID19
+//
+//  Created by 전해동 on 2020/02/27.
+//  Copyright © 2020 전해동. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
