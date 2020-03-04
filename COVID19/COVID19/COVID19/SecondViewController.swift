971e9e2499249a99e2a33b187afe99c9aa35eba7
Haedong-Jeon <21212004@ynu.ac.kr>
2020-03-04 23:46:45 +0900

    완치, 사망 %로 표현

--- COVID19/COVID19/COVID19/SecondViewController.swift
+++ COVID19/COVID19/COVID19/SecondViewController.swift
@@ -16,10 +16,9 @@ class SecondViewController: CustomViewController, ChartViewDelegate{
     var counter:Int = 0
     var values:Array<Double> = []
     var dataEntries:Array<ChartDataEntry> = []
-    var colors:Array<UIColor> = [UIColor.blue, UIColor.green, UIColor.red]
-    var labels:Array<String> = ["확진자", "격리해제", "사망자"]
+    var colors:Array<UIColor> = [UIColor.blue, UIColor.red]
+    var labels:Array<String> = ["완치", "사망"]
     var LegendEntries:Array<LegendEntry> = []
-    var dummy:CGSize = CGSize(width: CGFloat.nan, height: CGFloat.nan)
     override func viewDidLoad(){
         super.viewDidLoad()
         getDataForPieChart()
@@ -32,10 +31,10 @@ class SecondViewController: CustomViewController, ChartViewDelegate{
             dataEntry.label = labels[i]
             dataEntries.append(dataEntry)
         }
-        let pieChartDataSet = PieChartDataSet(entries: dataEntries)
+        let pieChartDataSet = PieChartDataSet(entries: dataEntries, label:"국내 동향")
         let pieChartData = PieChartData(dataSet: pieChartDataSet)
         chart?.data = pieChartData
-        chart?.legend.calculatedLabelSizes.append(dummy)//Chart 라이브러리의 LegendRenderer에서 calcuratedLabelSizes의 크기가 2로 고정돼있음 그래서 하나 늘림
+
         
         chart?.legend.entries = LegendEntries
         pieChartDataSet.colors = self.colors
@@ -58,12 +57,12 @@ class SecondViewController: CustomViewController, ChartViewDelegate{
         performSegue(withIdentifier: "goToCheckDangerLocation", sender: nil)
     }
     //이 화면은 내렸을 때 앱 종료
-//    override func viewWillDisappear(_ animated: Bool){
-//        super.viewWillDisappear(animated)
-//        if isBeingDismissed{
-//            UIControl().sendAction(#selector(URLSessionTask.suspend), to: UIApplication.shared, for: nil)
-//        }
-//    }
+    override func viewWillDisappear(_ animated: Bool){
+        super.viewWillDisappear(animated)
+        if isBeingDismissed{
+            UIControl().sendAction(#selector(URLSessionTask.suspend), to: UIApplication.shared, for: nil)
+        }
+    }
     //파이 차트를 그리기 위한 데이터를 얻기
     func getDataForPieChart(){
         let mainURL = "http://ncov.mohw.go.kr/bdBoardList_Real.do?brdId=1&brdGubun=11&ncvContSeq=&contSeq=&board_id=&gubun="
@@ -76,7 +75,7 @@ class SecondViewController: CustomViewController, ChartViewDelegate{
           let doc = try HTML(html: coronaStatus, encoding: .utf8)
             for htmlSource in doc.xpath(Constant.mainStatusTable){
                 if counter >= 3{
-                    //확진 환자, 격리 해제, 사망자 총 3가지 정보만 담기 위해서. 그외 불필요한 정보는 처낸다.
+                    //불필요한 정보는 처낸다.
                     break
                 }
                 let test = Observable<String?>.just(htmlSource.text)
@@ -85,18 +84,24 @@ class SecondViewController: CustomViewController, ChartViewDelegate{
                     .subscribe(onNext:{text in self.counter += 1; mainStatus.mainStatus.append(text!)})
                     .dispose()
         }
+            mainStatus.mainStatus.remove(at: 0)//맨앞 정보는 확진자 수. 너무 많아서 이제 별 의미 없다.
         }catch let error{
           print("Error: \(error)")
         }
     }
     
     func changeStringToDoubleForData(){
+        var sum = 0.0
         for i in 0...mainStatus.mainStatus.count-1{
             mainStatus.mainStatus[i] = mainStatus.mainStatus[i].trimmingCharacters(in: ["명"])
             mainStatus.mainStatus[i] = mainStatus.mainStatus[i].components(separatedBy: [","]).joined()
             let value = (mainStatus.mainStatus[i] as NSString).doubleValue
+            sum += value
             values.append(value)
             print(values[i])
+        }
+        for i in 0...values.count-1{
+            values[i] = (values[i] / sum) * 100
         }
     }
     func controllLegend(){
